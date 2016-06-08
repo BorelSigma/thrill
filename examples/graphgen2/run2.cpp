0a2955c44f31c7e45fe8729a6cdb58e96d9290a7
daemon7777 <tomzuegel@gmx.de>
2016-06-08 17:33:26 +0200

    Add Lineparser

--- examples/graphgen2/run2.cpp
+++ examples/graphgen2/run2.cpp
@@ -11,30 +11,65 @@
 #include <thrill/api/size.hpp>
 #include <thrill/api/print.hpp>
 
+#include <thrill/common/cmdline_parser.hpp>
+#include <thrill/common/logger.hpp>
+#include <thrill/common/string.hpp>
+
 
 using Edge = GraphGen2::Edge;
+using Node = GraphGen2::Node;
+using Degree = GraphGen2::Degree;
 
 static void runParallel(
-        thrill::Context& ctx,
-        GraphGen2::BAGraph<GraphGen2::CliqueGraph,GraphGen2::BAHash> graph) {
+        thrill::Context& ctx, uint32_t graph, Node seed_nodes, Node n, Degree d, std::string output) {
     ctx.enable_consume();
-    auto edges_dia = Generate(ctx, [graph](const size_t& index){
-        return graph(index);
-    }, 100).Collapse();
-    GraphGen2::MapEdges(edges_dia).Print("node | degree");
 
+	
+	GraphGen2::BAHash h;
+	GraphGen2::CliqueGraph clique(seed_nodes);
+	GraphGen2::BAGraph<GraphGen2::CliqueGraph,GraphGen2::BAHash> BA(n,d,clique,h);
+
+    auto edges_dia = Generate(ctx, [BA](const size_t& index){
+        return BA(index);
+    }, BA.number_of_edges()).Collapse();
+    auto degrees = GraphGen2::MapEdges(edges_dia);
+    //auto distribution = GraphGen2::ParallelDegreeDistribution(degrees);
+    degrees.Print(" ");
 }
 
 int main(int argc, char* argv[])
 {
-	GraphGen2::BAHash h;
-	auto s = 5;
-	GraphGen2::CliqueGraph clique(s);
-	GraphGen2::BAGraph<GraphGen2::CliqueGraph,GraphGen2::BAHash> BA2(100,3,clique,h);
+	thrill::common::CmdlineParser clp;
+
+	uint32_t graph = 0;
+
+    	clp.AddUInt('g', "graph", graph,
+                "0 : Path, 1 : Clique, 2 : Babarasi<Path>, 3 : Babarasi<Clique>");
+
+	GraphGen2::Node s = 0;
+	GraphGen2::Node n = 0;
+	GraphGen2::Degree d = 0;
+
+	clp.AddBytes('s', "seed_nodes",s,"number of nodes in seed graph");
+	clp.AddBytes('n', "maximum_nodes",n,"number of nodes in babarasi albert graph");
+	clp.AddBytes('d', "degree",d,"degree of each new node in babarasi albert graph");
+
+    	std::string output;
+    	clp.AddString('o', "output", output,
+                  "filename for png output ");
+
+
+    	if (!clp.Process(argc, argv)) 
+	{
+        	return -1;
+    	}
+
+    	clp.PrintResult();
+
 
 	auto start_func =
         [&](thrill::Context& ctx) {
-            runParallel(ctx, BA2);
+            runParallel(ctx, graph, s, n, d, output);
         };
 
 	thrill::Run(start_func);
